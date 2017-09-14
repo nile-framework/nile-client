a5aae0b733f9590ccbb7c10384f520b181897bc6
Kevin Towe <kevintowe27@gmail.com>
2017-09-14 13:22:58 -0600

    added the initial form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { AuthProvider } from '../../providers/auth/auth';
 
@@ -11,12 +16,19 @@ import { AuthProvider } from '../../providers/auth/auth';
 })
 export class HomePage {
 
+  initialInfoForm: FormGroup;
   public authorized;
+
+  // if the users initial company information hasn't been submitted yet, display a form asking for the information.
+  displayInitialCompanyForm: boolean = false;
+
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private _authProvider: AuthProvider
+    private _authProvider: AuthProvider,
+    private _fb: FormBuilder,
+    private _afAuth: AngularFireAuth
   ) {
     this._authProvider.authState.subscribe( authState => {
       if ( authState === true){
@@ -24,13 +36,15 @@ export class HomePage {
       } else {
         this.authorized = false;
       }
-    })
+    });
     
     this._authProvider.comPosition.subscribe( comPosition => {
       if (comPosition === 'owner') {
         this._authProvider.authState.subscribe( authState => {
           if (authState === 'initialSetup') {
             // we display a form for the company owner to fill out with basic info for the company.
+            this.buildInitialInfoForm();
+            this.displayInitialCompanyForm = true;
           }
         })
       } else if (comPosition === 'employee') {
@@ -43,8 +57,23 @@ export class HomePage {
     
   }
 
+  buildInitialInfoForm() {
+    this.initialInfoForm = this._fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.email],
+      phoneNumber: ['', Validators.minLength(10)],
+      deliveriesPerWeek: ['', Validators.required]
+    })
+
+    firebase.database().ref(``)
+  }
+
   startOrder() {
   this.navCtrl.setRoot('NewOrderPage');
+  }
+
+  onSubmitInitialInfoForm() {
+    
   }
 
 }
