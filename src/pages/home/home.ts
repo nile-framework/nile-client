e7425672ed9284dc3f549c328dee0cf00ba97527
Kevin Towe <kevintowe27@gmail.com>
2017-09-14 12:25:51 -0600

    In the home page we subscribe to multipe values from the auth provider in order to display the home page as should be viewed.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,12 +11,32 @@ import { AuthProvider } from '../../providers/auth/auth';
 })
 export class HomePage {
 
+  public authorized;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private _authProvider: AuthProvider
   ) {
-    // this._authProvider.
+    this._authProvider.authState.subscribe( authState => {
+      if ( authState === true){
+        this.authorized = true;
+      } else {
+        this.authorized = false;
+      }
+    })
+    
+    this._authProvider.comPosition.subscribe( comPosition => {
+      if (comPosition === 'owner') {
+        this._authProvider.authState.subscribe( authState => {
+          if (authState === 'initialSetup') {
+            // we display a form for the company owner to fill out with basic info for the company.
+          }
+        })
+      } else if (comPosition === 'employee') {
+
+      }
+    })
   }
 
   ionViewDidLoad() {
