a2deaeb1e2a9741a02ff8ed306c2a8b103a65f8f
Kevin Towe <kevintowe27@gmail.com>
2017-09-14 11:19:56 -0600

    import auth provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { AuthProvider } from '../../providers/auth/auth';
+
 
 @IonicPage()
 @Component({
@@ -11,8 +13,10 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public navParams: NavParams,
+    private _authProvider: AuthProvider
   ) {
+    // this._authProvider.
   }
 
   ionViewDidLoad() {
